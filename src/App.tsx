8ed17d30e0e07388af9dc2b7550171af96c98a7c
gitcrumbs <ashwanisingh034@gmail.com>
2022-10-11 01:40:32 +0530

    Frontend RPC

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import  {useState} from 'react'
 import './App.css';
+import { GreetServiceClient } from './greet.client';
+import { Greeting, GreetRequest } from './greet';
+import { GrpcWebFetchTransport } from '@protobuf-ts/grpcweb-transport';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default  function App() {
+
+ const [resp,setResp] = useState("");
+
+  const grpcCall = () => {
+
+    // create our greeting object
+    let greeting :  Greeting = {
+      firstName: "Ashwani",
+      lastName: 'Singh'
+    };
+
+    
+
+     // prepare the greet request
+    
+     let request :  GreetRequest= {
+      greeting
+     };
+     
+     
+     let transport = new GrpcWebFetchTransport({
+      baseUrl: "http://localhost:8080"
+  });
+
+  let client = new GreetServiceClient(transport);
+
+
+  let {response} =  client.greet(request, {});
+
+   response.then(resp=>{
+    setResp(resp.result);
+  }
+  
+   ).catch(err=>{
+    console.log(err);
+    
+   })
+
 }
 
-export default App;
+return (
+      <div>
+        <button onClick={grpcCall}>Click</button>
+        <p>{resp}</p>
+      </div>
+ );
+}
+
+
+
